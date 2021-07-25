d5b79d31be713ff25fe4552911240e8eda84b957
unknown <mbelhedi@outlook.com>
2021-07-25 09:03:05 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React,{useState}from 'react';
+// setState immer im import
+import './App.css';
+import { getSunrise, getSunset } from 'sunrise-sunset-js';
+const api={
+  key:'8d3a4c2ff73ef6bcb19e913e835f26c7',
+  base:'https://api.openweathermap.org/data/2.5/'
+
+}
+function App() {
+ 
+  const [query,setQuery]=useState('');
+  const [wetter,setWetter]=useState({});
+  const[sonneuntergang,setSonnenUntergang]=useState({});
+// bitte anwenden der Node js kurs app mit den Geodaten
+  const suche=evt=>{
+    if(evt.key==="Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}&lang=de`)
+      .then(res=>res.json())
+      .then(result=>{
+        setWetter(result);
+        setQuery('');
+        setSonnenUntergang(result);
+        console.log(result)
+      
+      });
+    }
+  }
+
+  const Datum=(d)=>{
+    let monate=["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","Novembe","Dezember"]
+    let tage=["Sonntag","Montag","Dienstg","Mittwoch","Donnerstag","Freitag","Samstag"]
+    
+  // denke die Get der Monate immer auf english kein Dotsch 😁
+    let tag=tage[d.getDay()];
+    let datum=d.getDate();
+    let monat=monate[d.getMonth()];
+    let jahr=d.getFullYear();
+    
+
+    return`${tag} ${datum} ${monat} ${jahr}`
+    
+  
+  }
+
+
+  return (
+    <div className={(typeof wetter.main!="undefined")? ((wetter.main.temp < 16)? 'app deser':'app'):'app'}>
+     <main>
+     <div className="suchbox">
+       <input 
+       type="text"
+       className="suchanfrage"
+       placeholder="suchen..." 
+       onChange={e=>setQuery(e.target.value)}
+       value={query}
+       onKeyPress={suche}
+       
+       />
+     </div>
+    
+        {(typeof wetter.main !="undefined") ?(
+       <div>
+
+<div className="location-box">
+        <div className="location">{wetter.name},{wetter.sys.country}
+          <div className="date">{Datum(new Date())}
+
+          </div>
+        </div>
+        </div>
+      
+      <div className="wetterbox">
+        <div className="temp">
+          {Math.round(wetter.main.temp)}°C
+        </div>
+        <div className="wetter">
+          {wetter.weather[0].description}
+          
+</div>
+<div className="sonnenaufgang">
+  <div className="sonneauf">Sonnenaufgang {new Date(wetter.sys.sunrise*1000).toLocaleTimeString('de')}</div>
+  {wetter.weather.description}
+</div>
+<div className="sonnenuntergang">
+  <div className="sonneunter">Sonnenuntergang {new Date(wetter.sys.sunset*1000).toLocaleTimeString('de')}</div>
+  {wetter.weather.description}
+</div>
+        </div>
+        <br />
+        <div className="windgesch">
+  <div className="wind">Feuchtigkeit 🌫 {Math.round(wetter.main.humidity)} %</div>
+  {wetter.weather.description}
+</div>
+<div className="windgesch">
+  <div className="wind">Wind 🌪 {Math.round(wetter.wind.speed)} Km/h</div>
+  {wetter.weather.description}
+</div>
+<div className="windgesch">
+  <div className="wind">Gefühlt {Math.round(wetter.main.feels_like)} °C</div>
+  {wetter.weather.description}
+</div>
+       </div>
+      
+      ):('')}
+     </main>
+    </div>
+  );
+}
+
+export default App;
